271a73e110c091eeb5d89185da43c5c8935e2164
yz398 <root@DESKTOP-3IJBH7Q.localdomain>
2018-02-10 23:50:42 -0500

    min_max

---
+++ min_max_list.py
@@ -0,0 +1,13 @@
+
+# coding: utf-8
+
+# In[37]:
+
+
+def min_max_list(your_list):
+    my_min = min(your_list)
+    my_max = max(your_list)
+    my_min_max = (my_min,my_max)
+    
+    return my_min_max
+
