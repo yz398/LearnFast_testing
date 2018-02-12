8e5d4a5deb61dc0719bb3f1ea23a9245a8fd325d
anika-mukherji <anika.mukherji@duke.edu>
2018-02-12 10:12:15 -0500

    minor changes, tests now passing

--- min_max_list.py
+++ min_max_list.py
@@ -1,4 +1,7 @@
 def min_max_list(x):
+
+    if not x:
+        return (None, None)
     my_min = min(x)
     my_max = max(x)
     my_min_max = (my_min,my_max)
--- test_min_max_list.py
+++ test_min_max_list.py
@@ -1,17 +1,21 @@
 import pytest
+from min_max_list import min_max_list
 
-def test_min_max_list(x):
+def test_min_max_list():
     
     test_data1 = [0,-3,-1.2,10]
     test_data2 = [1,3,2,5]
     test_data3 = [-3/2,-9,-3,-7,-1]
+    test_data4 = []
     test_answer1 = (-3,10)
     test_answer2 = (1,5)
     test_answer3 = (-9,-1)
+    test_answer4 = (None, None)
     
     assert test_answer1 == min_max_list(test_data1)
     assert test_answer2 == min_max_list(test_data2)
     assert test_answer3 == min_max_list(test_data3)
+    assert test_answer4 == min_max_list(test_data4)
 
     
 
