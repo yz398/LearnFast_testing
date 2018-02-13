cc0a9d709df61f293c6f44ad00d1e2a32562448b
anika-mukherji <anika.mukherji@duke.edu>
2018-02-13 16:48:32 -0500

    resolving conflict with test file

--- list_module/sum_list.py
+++ list_module/sum_list.py
@@ -1,4 +1,60 @@
-
 def sum_list(x):
+    """
+    Returns the sum of a list
 
-    return sum(x)
+    :param x: list to be summed
+    :type x: list
+    :raises EmptyListException: if empty list is passed in
+    :raises DictionaryException: if dict is passed in
+    :return: sum of list
+    :rtype: float
+    """
+    try:
+        import logging
+    except ImportError:
+        print("Could not import logging module")
+        return
+    else:
+        logging.basicConfig(filename="./sum_list.log", filemode='w', 
+                level=logging.DEBUG)
+    if not x: 
+        raise EmptyListException()
+        logging.warning("Passed in an empty list")
+        return None
+    if type(x) == dict:
+        raise DictionaryException()
+        logging.warning("Passed in a dictionary")
+        return None
+    try:
+        logging.info("Trying to find the sum")
+        the_sum = sum(x)
+    except TypeError:
+        logging.error("Inccorect parameter type passed in")
+        return None
+    else:
+        return float(the_sum)
+
+class CustomListExceptions(BaseException):
+    """
+    Custom set of exceptions for this list module
+    """
+    def __init__(self, message=""):
+        """
+        Creating an instance of an exception
+
+        :param message: optional message to explain exception
+        :type message: string
+        """
+        self.message = message
+
+class EmptyListException(CustomListExceptions):
+    """
+    Exception to be thrown if empty list is passed in
+    """
+    pass
+
+class DictionaryException(CustomListExceptions):
+    """
+    Exception to be thrown if dictionary is passed in
+    """
+    pass
--- test_sum_list.py
+++ test_sum_list.py
@@ -1,14 +1,18 @@
 import pytest
 
-from list_module.sum_list import sum_list
+from list_module.sum_list import (sum_list, EmptyListException, 
+DictionaryException)
 
 
 def test_sum_list():
-    test_data = (
-            [1, 1, 1, 1], [0, 0], [],
-            [5, -1, -10], [-9, -4],
-            [1000, 2000, -1]
-            )
-    test_answers = ([4, 0, 0, -6, -13, 2999])
+    test_data = ([1, 1, 1, 1], [0, 0], [5, -1, -10], [-9, -4], [1000, 2000, -1])
+    test_answers = ([4, 0, -6, -13, 2999])
     for (example, ans) in zip(test_data, test_answers):
         assert sum_list(example) == ans
+    test_type_fails = ("BME", 590, 3.0)
+    for type_fail in test_type_fails:
+        assert sum_list(type_fail) == None
+    with pytest.raises(EmptyListException):
+        sum_list([])
+    with pytest.raises(DictionaryException):
+        sum_list({1: 'a', 2: 'b'})
