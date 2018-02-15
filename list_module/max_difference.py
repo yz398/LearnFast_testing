f30c030513244db8d1ce38cb36915786c09f0ec3
apple <apple@ZhiweideMacBook.local>
2018-02-15 15:08:35 -0500

    assignment5_zhiwei2

--- list_module/max_difference.py
+++ list_module/max_difference.py
@@ -6,28 +6,26 @@ def max_difference(x):
         :type x: list
         :raises TypeError: if input in is not a list
         :raises ValueErrorException: if the list contain non-number
+
         :return: the maximum difference of adjacent numbers
         :rtype: float
         """
-    
-    import logging
+    try:
+        import logging
+        from math import fabs
+    except ImportError:
+        print('you import filed')
     logging.basicConfig(filename='max_difference.log', filemode='w', level=logging.DEBUG)
-    
-    from math import fabs
-    
     if type(x) is not list:
         logging.error('Input is not a list')
-        raise TypeError()
-    
+        raise TypeError
     curr_max = 0.0
-    
     for index, number in enumerate(x):
         try:
             float(number)
         except ValueError:
-            print('your input have a invalid number')
+            print('your input have a invalid number', number)
             return
-        
         if index > 0:
             diff = fabs(x[index] - x[index - 1])
             curr_max = max(curr_max, diff)
--- test_max_difference.py
+++ test_max_difference.py
@@ -7,17 +7,27 @@ def test_max_difference():
     try:
         from list_module.max_difference import max_difference
     except ImportError:
-        print('you import filed')
+        print("The necessary module for this test failed to import")
 
-    test_data1 = [1, 5, 6, 7, 12]
-    test_data2 = [3, 4, 5, 56, 45]
-    test_data3 = [6, 34, 23, 45, 57]
+    test_data1 = [1,5,6,7,12]
+    test_data2 = [3,4,5,56,45]
+    test_data3 = [6,34,23,45,57]
     test_data4 = []
+    test_data5 = 1
+    test_data6 = [1,'b',7]
     output1 = max_difference(test_data1)
     output2 = max_difference(test_data2)
     output3 = max_difference(test_data3)
     output4 = max_difference(test_data4)
+    output5 = max_difference(test_data5)
+    output6 = max_difference(test_data6)
     assert output1 == 5
     assert output2 == 51
     assert output3 == 28
     assert output4 == 0
+    assert output5 is None
+    with pytest.raises(TypeError):
+        max_difference(2)
+    assert output6 is None
+    with pytest.raises(ValueError):
+        max_difference(['c',2,4])
