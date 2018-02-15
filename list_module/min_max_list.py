9931df30160b19ff4414773c82204ae7853e8d4f
yz398 <yz@DESKTOP-3IJBH7Q.localdomain>
2018-02-15 15:18:41 -0500

    more tests

--- list_module/min_max_list.py
+++ list_module/min_max_list.py
@@ -2,9 +2,9 @@ def min_max_list(x):
     """
     find the minimum and the maximum of a list and return a tuple
     :param x: the input should be a list
-    :raises ImportError: if the module not found
-    :raises TypeError: if the input is not a list or the input includes string
-    :raises ValueError:if the input includes string
+    :raises ImportError:  if the module not found
+    :raises TypeError:  if the input is not a list or the input includes string
+    :raises ValueError: if the input includes string
     
     :returns: return a tuple include minimum and maximum of the list 
     :rtype: (float,float)
@@ -17,21 +17,24 @@ def min_max_list(x):
         logging.basicConfig(filename='example.log', level=logging.DEBUG, 
                             filemode='w')   
     if type(x) is not list:
-        logging.warning('Watch out!The input should be list')
+        logging.error('Watch out!The input should be list')
         raise TypeError('TypeError with the input')
     for index in x:
         try:
             float(index)
         except ValueError as err:
             print('ValueError\n', err)
+            raise ValueError('change the input')
             logging.debug('if the type is string, it will let us know')
             return
         except TypeError as err:
-            logging.error('the type of index is wrong')
+            raise TypeError('change the in put style')
+            logging.debug('the type of index is wrong')
             print('TypeError\n', err)
             return    
     if not x:
         return (None, None)
+        logging.warning('no index there')
     my_min = min(x)
     my_max = max(x)
     my_min_max = (my_min, my_max)
--- test_min_max_list.py
+++ test_min_max_list.py
@@ -9,16 +9,26 @@ def test_min_max_list():
         from list_module.min_max_list import min_max_list
     except ImportError as err:
         print('big problem\n', err)
-        return         
+        return 
+    with pytest.raises(ValueError):
+        return_error([])
+    with pytest.raises(TypeError):
+        return_error(['s', 's']) 
     test_data1 = [0, -3, -1.2, 10]
     test_data2 = [1, 3, 2, 5]
     test_data3 = [-3/2, -9, -3, -7, -1]
     test_data4 = []
+    test_data5 = [1,2,'ag']
+    test_data6 = (1,2,3)
     test_answer1 = (-3, 10)
     test_answer2 = (1, 5)
     test_answer3 = (-9, -1)
     test_answer4 = (None, None)
+    test_answer5 = []
+    test_answer6 = ['s','s']
     assert test_answer1 == min_max_list(test_data1)
     assert test_answer2 == min_max_list(test_data2)
     assert test_answer3 == min_max_list(test_data3)
     assert test_answer4 == min_max_list(test_data4)
+    assert test_answer5 == min_max_list(test_data5)
+    assert test_answer6 == min_max_list(test_data6) 
